7299f68b1fac3e8291ccb5800a200dd28ed8b8fb
adriansotte <adrianmvparedes@gmail.com>
2025-03-14 14:12:18 +0100

    Cambio de modelos y propiedades del proyecto

--- Retos/Reto 1/BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/BananaWhatsAppApplication.java
+++ Retos/Reto 1/BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/BananaWhatsAppApplication.java
@@ -1,7 +1,16 @@
 package com.banana.bananawhatsapp;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories("com.banana.bananawhatsapp.persistencia")
+@EntityScan("com.banana.bananawhatsapp.persistencia")
 public class BananaWhatsAppApplication {
 
     public static void main(String[] args) {
+
+        System.out.println("Mensajeria de whatsapp");
     }
 }
