71d3baad7f7f23b94ada8bc4821e1cf9ed717b62
adriansotte <adrianmvparedes@gmail.com>
2025-03-14 14:56:20 +0100

    Implementación del servicio de usuario

--- Retos/Reto 1/BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/servicios/IServicioUsuarios.java
+++ Retos/Reto 1/BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/servicios/IServicioUsuarios.java
@@ -4,16 +4,17 @@ import com.banana.bananawhatsapp.exceptions.UsuarioException;
 import com.banana.bananawhatsapp.modelos.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.SQLException;
 import java.util.Set;
 
 public interface IServicioUsuarios  {
     public Usuario obtener(int id) throws UsuarioException;
 
-    public Usuario crearUsuario(Usuario usuario) throws UsuarioException;
+    public Usuario crearUsuario(Usuario usuario) throws UsuarioException, SQLException;
 
     public boolean borrarUsuario(Usuario usuario) throws UsuarioException;
 
-    public Usuario actualizarUsuario(Usuario usuario) throws UsuarioException;
+    public Usuario actualizarUsuario(Usuario usuario) throws UsuarioException, SQLException;
 
     public Set<Usuario> obtenerPosiblesDesinatarios(Usuario usuario, int max) throws UsuarioException;
 }
--- Retos/Reto 1/BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/servicios/ServicioUsuarioImpl.java
+++ Retos/Reto 1/BananaWhatsApp/src/main/java/com/banana/bananawhatsapp/servicios/ServicioUsuarioImpl.java
@@ -6,6 +6,8 @@ import com.banana.bananawhatsapp.persistencia.IUsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -16,26 +18,43 @@ public class ServicioUsuarioImpl implements IServicioUsuarios{
 
     @Override
     public Usuario obtener(int id) throws UsuarioException {
-        return null;
+        // Se podria hacer con el optional pero hay que cambiar el metodo de la capa de persistencia.
+        Usuario usuario = ur.getById(id);
+        if(usuario != null) {
+            return usuario;
+        } else {
+            return null;
+        }
+
     }
 
     @Override
-    public Usuario crearUsuario(Usuario usuario) throws UsuarioException {
-        return null;
+    public Usuario crearUsuario(Usuario usuario) throws UsuarioException, SQLException {
+        ur.crear(usuario);
+        return usuario;
     }
 
     @Override
-    public boolean borrarUsuario(Usuario usuario) throws UsuarioException {
-        return false;
+    public boolean borrarUsuario(Usuario usuario) {
+        try{
+            ur.borrar(usuario);
+            return true;
+        } catch(SQLException e) {
+            return false;
+        }
+
     }
 
     @Override
-    public Usuario actualizarUsuario(Usuario usuario) throws UsuarioException {
-        return null;
+    public Usuario actualizarUsuario(Usuario usuario) throws UsuarioException, SQLException {
+        return ur.actualizar(usuario);
     }
 
     @Override
     public Set<Usuario> obtenerPosiblesDesinatarios(Usuario usuario, int max) throws UsuarioException {
-        return Set.of();
+        List<Usuario> listaUsuarios = ur.findAll();
+        //for(numero : max) {
+        //}
+        return (Set<Usuario>) listaUsuarios;
     }
 }
